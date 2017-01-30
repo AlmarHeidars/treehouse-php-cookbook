f98cc7b42771b91c836f92be3e3ff867d39822fa
Almar Heidarsson <almarheidars@gmail.com>
2017-01-30 16:39:49 -0500

    initial commit of cookbook off treehouse website, classes objects and adding properties

---
+++ classes/recipe.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Recipe
+{
+  public $title;
+  public $ingredients = array();
+  public $instructions = array ();
+  public $yield;
+  public $tag = array();
+  public $source = "Alena Holligan";
+}
+
+$recipe1 = new Recipe();
+
+var_dump($recipe1);
+
+?>
